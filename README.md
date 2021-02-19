baf084da08a87abc04f562470f70697fdd6a8038
northwestwitch <rasi.chiara@gmail.com>
2021-02-19 15:51:01 +0100

    fixed test and now looks like it's working

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # pdfmerger
 A small python package to merge and bookmark PDF files based on PyPDF2 and reportlab.
 
-Allows you to merge all PDF files present in a directory after writing a footer on each file the name of the original file.
+Allows you to merge a list of PDF files after adding a watermark on each with the name of the original file.
 
 Creates also bookmarks to the original files.
--- pdfmerger/pdfmerger.py
+++ pdfmerger/pdfmerger.py
@@ -8,20 +8,28 @@ import tempfile
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 
-
-def create_footer(footer_text):
+def create_watermark(text):
     """Add a string of text to a PDF file"""
+    (width, height) = letter
     packet = io.BytesIO()
     can = canvas.Canvas(packet, pagesize=letter)
-    can.drawString(25, 25, f" -- {footer_text} --")
+    can.setFont('Helvetica', 15)
+    can.drawCentredString(width / 2.0, width + 120, f" ## {text} ##")
     can.save()
     # move to the beginning of the StringIO buffer
     packet.seek(0)
     return packet
 
+def validate_infiles(infiles):
+    """Checks that each privided file exists on disk"""
+    for afile in infiles:
+        if os.path.isfile(afile):
+            continue
+        click.echo(f"Incorrect path to file: {afile}")
+        raise click.Abort()
 
 @click.command()
-@click.argument("infolder", type=click.Path(exists=True), required=True)
+@click.option('--infile', '-f', required=True, multiple=True, help="Path to one infile, repeat for multiple files")
 @click.option(
     "--outfolder",
     type=click.Path(exists=True),
@@ -30,54 +38,59 @@ def create_footer(footer_text):
     help="Path to destination outfile",
 )
 @click.option(
-    "--outname",
+    "--outfile",
     type=click.STRING,
     required=False,
     nargs=1,
     help="Name of destination outfile",
     default="pdfmerger_out.pdf",
 )
-def concatenate(infolder, outfolder, outname):
-    """Concatenate all PDF files in a folder and add bookmarks and file footers with original PDF name"""
+def concatenate(infile, outfolder, outfile):
+    """Concatenate all PDF files in a folder and add bookmarks and file watermarks containing with original PDF name"""
 
-    pdf_files = sorted(glob.glob(os.path.join(infolder, "*.pdf")))
+    validate_infiles(infile) # infile is actually a list of files
 
-    # Create a list of tuples consisting in path to PDF with corresponding bookmark name
-    files_n_filenames = [
-        (pdf_file, os.path.basename(pdf_file).split(".")[0]) for pdf_file in pdf_files
+    # Create a list of tuples consisting in path to PDF with corresponding bookmark (file) name.
+    # Example: [('pdfmerger/demo/pdf1.pdf', 'pdf1'), ('pdfmerger/demo/pdf2.pdf', 'pdf2')
+    filepaths_n_filenames = [
+        (pdf_file, os.path.basename(pdf_file).split(".")[0]) for pdf_file in infile
     ]
 
     # merge PDF files into one, with bookmarks
     merged = PdfFileMerger()
 
     # Loop over PDF files contained in folder
-    for item in files_n_filenames:
-
-        # Create a new temp file with PDF footer
-        footer_content = create_footer(item[1])
-        tmp_footer = tempfile.NamedTemporaryFile(delete=True)
-        tmp_footer = PdfFileReader(footer_content)
+    for filepath, filename in filepaths_n_filenames:
 
         # Read original PDF
-        existing_pdf = PdfFileReader(open(item[0], "rb"))
+        existing_pdf = PdfFileReader(open(filepath, "rb"))
+
+        # Create a new temp file with PDF footer
+        wmark = create_watermark(filename)
+        wmark_pdf = tempfile.NamedTemporaryFile(delete=True)
+        wmark_pdf = PdfFileReader(wmark)
 
         # add the "watermark" (which is the new pdf) on the existing page
         output = PdfFileWriter()
-        page = existing_pdf.getPage(0)
-        page.mergePage(tmp_footer.getPage(0))
+        page = wmark_pdf.getPage(0)
+        page.mergePage(existing_pdf.getPage(0))
         output.addPage(page)
 
         # write "output" to a new file
-        tmp_pdf_with_footer = tempfile.NamedTemporaryFile(delete=True)
-        outputStream = open(tmp_pdf_with_footer.name, "wb")
+        tmp_pdf_with_wmark = tempfile.NamedTemporaryFile(delete=True)
+        outputStream = open(tmp_pdf_with_wmark.name, "wb")
         output.write(outputStream)
         outputStream.close()
 
-        # Append each temp PDF file with footer to the merged final PDF
-        merged.append(tmp_pdf_with_footer.name, bookmark=item[1])
+        # Append each temp PDF file with watermark to the merged final PDF
+        merged.append(tmp_pdf_with_wmark.name, bookmark=filename)
 
     # Write merged file
-    merged.write(os.path.join(outfolder or "", outname))
+    if outfile.endswith(".pdf") is False:
+        outfile += ".pdf"
+    outpath = os.path.join(outfolder or "", outfile)
+    click.echo(f"Writing outfile to: '{outpath}'")
+    merged.write(outpath)
 
 
 def main():
